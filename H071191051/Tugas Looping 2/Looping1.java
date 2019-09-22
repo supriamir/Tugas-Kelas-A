6fe3eefc31b00374a4a7d3e9b20db5de2dd72f11
fajri-rasid1st <fajrimuhammad460@gmail.com>
2019-09-22 13:33:46 +0700

    Tugas Looping 2 telah ditambahkan

---
+++ H071191051/Tugas Looping 2/Looping1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Looping1{
+    public static void main(String[] args){
+        Scanner userInput = new Scanner(System.in);
+        int nilaiKhusus = 2002;
+        boolean kondisi = true;
+        while (kondisi){
+            System.out.print("Masukkan password = ");
+            int password = userInput.nextInt();
+            if (password != nilaiKhusus){
+                System.out.println("Password Salah");
+            }else{
+                System.out.println("Password Benar");
+                kondisi = false;
+            }
+        }
+        userInput.close();
+    }
+}
