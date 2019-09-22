49bf4b7d0ed3964e97fe219cea7196d717bb038e
Muhammad Fajri Rasid <54720865+fajri-rasid1st@users.noreply.github.com>
2019-09-22 12:34:31 +0700

    Update and rename Looping2.java to LoopingSatu.java

--- H071191051/Tugas Looping 2/LoopingSatu.java
+++ H071191051/Tugas Looping 2/LoopingSatu.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class LoopingLima{
+public class LoopingSatu{
     public static void main(String[] args){
         Scanner userInput = new Scanner(System.in);
         int base = userInput.nextInt();
