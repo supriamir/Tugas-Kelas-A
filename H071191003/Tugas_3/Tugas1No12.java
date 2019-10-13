c20701d4b850b464932b96e0db58004008f2a22e
PandaNuisance <azizarsyi@gmail.com>
2019-10-13 20:27:06 +0800

    Added constraints

--- H071191003/Tugas_3/Tugas1No12.java
+++ H071191003/Tugas_3/Tugas1No12.java
@@ -4,17 +4,29 @@ class Tugas1No12{
     Scanner input = new Scanner(System.in);
     System.out.print("Input jumlah sepatu yang ada di toko : ");
     int n =input.nextInt();
+    if(n<1 || n>100000){
+      System.out.println("Jumlah sepatu yang diinput invalid!");
+      return;
+    }
     int shoeSize[] = new int[n];
     boolean available = false;
     String shoeColour[] = new String[n];
     for (int i = 0;i<n ;i++ ) {
       System.out.printf("Input ukuran sepatu ke-%d : ", i+1);
       shoeSize[i] = input.nextInt() ;
+      if(shoeSize[i]<1 || shoeSize[i]>100000){
+        System.out.println("Ukuran sepatu yang diinput invalid!");
+        return;
+      }
       System.out.printf("Input warna sepatu ke-%d : ", i+1);
       shoeColour[i] = input.next();
     }
     System.out.printf("\nInput ukuran sepatu yang ingin dicari : ");
     int desiredShoeSize = input.nextInt();
+    if(desiredShoeSize<1 || desiredShoeSize>100000){
+      System.out.println("Ukuran sepatu yang diinput invalid!");
+      return;
+    }
     System.out.printf("Input warna sepatu yang ingin dicari : ");
     String desiredShoeColour = input.next();
     for (int i=0;i<n ;i++ ) {
